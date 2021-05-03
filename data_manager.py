43a22cdda6181a31fbeeb6cf1437b63ad6fbd340
juliamullen <julia.l.mullen@gmail.com>
2021-05-03 18:24:37 -0400

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -7,7 +7,7 @@ daily_report_re = re.compile('\d\d-\d\d-\d\d\d\d.csv')
 class DataManager:
     def __init__(self, source):
         if source == 'JHU-daily':
-            daily_report_folder = 'COVID-19/csse_covid_19_data/csse_covid_19_daily_reports_us/'
+            daily_report_folder = 'COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/'
             dailies = []
             for report_filename in os.listdir(daily_report_folder):
                 if daily_report_re.search(report_filename):
