c0708230c25761db27687a98591e21ec18d96564
Kaleigh Jaeger <kaleighjaeger@kaleighs-mbp.myfiosgateway.com>
2021-06-16 14:14:12 -0400

    Adding load_to_memory() function and file to repository

--- load_to_memory.py
+++ load_to_memory.py
@@ -1,11 +1,17 @@
 import pandas as pd 
 
+#Remove filename
+#loop through the COVID-19 folder using OS
 def load_to_memory(filename):
-	data = pd.read_csv(f'{filename}') 
-	json = data.to_json(path_or_buf='result.json')
-	df = pd.DataFrame(data=data)
-	print('DataFrame: ')
-	print(df)
-	
-daily_report = 'COVID-19/csse_covid_19_data/csse_covid_19_daily_reports_us/01-01-2021.csv'
-load_to_memory(daily_report)
+    data = pd.read_csv(f'{filename}', header=0) 
+
+    print(data[0])
+    print(data[1])
+    return data
+
+# time_series = '/Users/kaleighjaeger/Scripps/epidemiology/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv''
+# load_to_memory(time_series)
+
+
+#json = data.to_json(path_or_buf='result.json')
+#print('DataFrame: ')
